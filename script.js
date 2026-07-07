7a186f20f900d27dbc1bc4ea7779e810fde59200
Jona909090 <jonic.ctba@gmail.com>
2026-07-07 22:02:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    setTimeout(function () {
+        document.body.classList.remove("not-loaded");
+    }, 1000);
+
+    setTimeout(function () {
+        document.getElementById("message").classList.add("show");
+    }, 3000);
+
+};
